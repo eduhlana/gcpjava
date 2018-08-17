e619e11aa6aa540583a39e9ecc22e8faced11652
eduhlana <mesquitalana@gmail.com>
2018-08-17 10:37:17 -0300

    Update BaseTest.java

--- src/main/java/core/BaseTest.java
+++ src/main/java/core/BaseTest.java
@@ -12,8 +12,7 @@ public class BaseTest {
     @Before
      public void Inicializa()
        {
-            //DriverFactory.GetDriver().navigate().to("http://hml3-naturahml.sysmap.com.br/gcpweb/");
-            DriverFactory.GetDriver().navigate().to("http://gcphml.natura.net/gcpweb/pages/preco/pesquisar");
+            DriverFactory.GetDriver().navigate().to("http:///eb/pages/preco/pesquisar");
             login.Logar();
             DriverFactory.GetDriver().manage().window().maximize();
             
